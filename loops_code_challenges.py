d08f771e455424703a21ef5a7334081d2ed547f5
Kevin Botero <kbotero201@gmail.com>
2021-04-29 18:53:06 -0400

    loops code challenges

---
+++ loops_code_challenges.py
@@ -0,0 +1,35 @@
+# Python Loops Code Challenges
+
+# Larger Sum 
+# Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+# The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+
+
+
+# Over 9000
+# Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+# The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+# For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+
+
+
+# Max Num 
+# Create a function named max_num() that takes a list of numbers named nums as a parameter.
+# The function should return the largest number in nums
+# Without using max()
+
+
+
+# Same Values 
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+# For example, the following code should return [0, 2, 3]
+# same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+
+
+
+# Reversed List 
+# Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+# The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+# For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
