87abc7357317c685e7f250df313802c80c7fdfd2
Kevin Botero <kbotero201@gmail.com>
2021-04-29 18:59:26 -0400

    functions code challenges

---
+++ functions_code_challenges.py
@@ -0,0 +1,27 @@
+# Python Function Code Challenges
+
+# First Three Multiples
+# Write a function named first_three_multiples() that has one parameter named num.
+# This function should print the first three multiples of num. Then, it should return the third multiple.
+# For example, first_three_multiples(7) should print 7, 14, and 21 on three different lines, and return 21.
+
+
+# Tip 
+# Create a function called tip() that has two parameters named total and percentage.
+# This function should return the amount you should tip given a total and the percentage you want to tip.
+
+
+# Dog Years 
+# Some say that every one year of a human’s life is equivalent to seven years of a dog’s life. Write a function named dog_years() that has two parameters named name and age.
+# The function should compute the age in dog years and return the following string:
+# "{name}, you are {age} years old in dog years"
+
+
+# All Operations 
+# Create a function named lots_of_math(). This function should have four parameters named a, b, c, and d. The function should print 3 lines and return 1 value.
+# First, print the sum of a and b.
+# Second, print c minus d.
+# Third, print the first number printed, multiplied by the second number printed.
+# Finally, return the third number printed modulo a.
+
+
