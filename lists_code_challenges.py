d877acb084f808c5e66a74abc3cd512979b299a2
Kevin Botero <kbotero201@gmail.com>
2021-04-29 18:42:17 -0400

    2 completed

--- lists_code_challenges.py
+++ lists_code_challenges.py
@@ -1,4 +1,4 @@
-# Easy List Code Challenges 
+# Python List Code Challenges 
 
 # Every Three Numbers 
 # Create a function called every_three_nums that has one parameter named start.
@@ -10,6 +10,8 @@ def every_three_nums(start):
   else:
     return (list(range(start, 101, 3)))
 
+print(every_three_nums(91))
+
 # Remove Middle 
 # Create a function named remove_middle which has three parameters named lst, start, and end.
 # The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
@@ -18,6 +20,8 @@ def every_three_nums(start):
 
 def remove_middle(lst, start, end):
   return lst[:start] + lst[end+1:]
+
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
 
 
 # More Frequent Item 
@@ -31,5 +35,40 @@ def more_frequent_item(lst, item1, item2):
   else:
     return item2
 
-    
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
+
+# Double Index 
+# Create a function named double_index that has two parameters: a list named lst and a single number named index.
+# The function should return a new list where all elements are the same as in lst except for the element at index. The element at index should be double the value of the element at index of the original lst.
+# If index is not a valid index, the function should return the original list.
+# For example, the following code should return [1,2,6,4] because the element at index 2 has been doubled:
+# double_index([1, 2, 3, 4], 2)
+
+def double_index(lst, index):
+  # Checks to see if index is too big
+  if index >= len(lst):
+    return lst
+  else:
+    # Gets the original list up to index
+    new_lst = lst[0:index]
+ # Adds double the value at index to the new list 
+  new_lst.append(lst[index]*2)
+ # Adds the rest of the original list
+  new_lst = new_lst + lst[index+1:]
+  return new_lst
+
+print(double_index([3, 8, -10, 12], 2))
+
+# Middle Item 
+# Create a function called middle_element that has one parameter named lst.
+# If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+
+def middle_element(lst):
+  if len(lst) % 2 == 0:
+    sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
+    return sum / 2
+  else:
+    return lst[int(len(lst)/2)]
+
+print(middle_element([5, 2, -10, -4, 4, 5]))
 
