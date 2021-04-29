8c0a255d0d897267f9f35e59ecd870a9d0f5f9f7
Kevin Botero <kbotero201@gmail.com>
2021-04-29 19:13:04 -0400

    dict code challenges

---
+++ dictionaries_code_challenges.py
@@ -0,0 +1,29 @@
+# Python Dictionaries Code Challenges 
+
+# Word Length Dict
+# Write a function named word_length_dictionary that takes a list of strings named words as a parameter. 
+# The function should return a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
+
+
+
+# Frequency Count
+# Write a function named frequency_dictionary that takes a list of elements named words as a parameter. 
+# The function should return a dictionary containing the frequency of each element in words.
+
+
+
+# Unique Values
+# Create a function named unique_values that takes a dictionary named my_dictionary as a parameter. 
+# The function should return the number of unique values in the dictionary.
+
+
+
+# Count First Letter
+# Create a function named count_first_letter that takes a dictionary named names as a parameter. names should be a dictionary where the key is a last name and the value is a list of first names. For example, the dictionary might look like this:
+# names = {"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}
+# The function should return a new dictionary where each key is the first letter of a last name, and the value is the number of people whose last name begins with that letter.
+# So in example above, the function would return:
+# {"S" : 4, "L": 3}
+
+
+
