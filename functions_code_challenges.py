bda2774a141e7c472676a45c8df6a091ddbf6c9c
Kevin Botero <kbotero201@gmail.com>
2021-05-04 08:34:34 -0400

    tip CC completed

--- functions_code_challenges.py
+++ functions_code_challenges.py
@@ -5,6 +5,14 @@
 # This function should print the first three multiples of num. Then, it should return the third multiple.
 # For example, first_three_multiples(7) should print 7, 14, and 21 on three different lines, and return 21.
 
+def first_three_multiples(num):
+  print(num)
+  print(num * 2)
+  print(num * 3)
+  return num * 3
+
+first_three_multiples(10)
+first_three_multiples(0)
 
 # Tip 
 # Create a function called tip() that has two parameters named total and percentage.
