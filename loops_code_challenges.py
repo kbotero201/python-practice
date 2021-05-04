defa2ccbfc815f5c1961be069c65c3d65230a3ca
Kevin Botero <kbotero201@gmail.com>
2021-05-04 08:33:19 -0400

    reversed list completed

--- loops_code_challenges.py
+++ loops_code_challenges.py
@@ -68,3 +68,12 @@ print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
 # Reversed List 
 # Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
 # The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):
+    if lst1[index] != lst2[len(lst2) - 1 - index]:
+      return False
+  return True
+
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
