67e5d348a8e3e25cf2711775817a92ea21bb30c0
Kevin Botero <kbotero201@gmail.com>
2021-05-05 12:20:29 -0400

    word cc completed

--- dictionaries_code_challenges.py
+++ dictionaries_code_challenges.py
@@ -4,6 +4,14 @@
 # Write a function named word_length_dictionary that takes a list of strings named words as a parameter. 
 # The function should return a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
 
+def word_length_dictionary(words):
+  word_lengths = {}
+  for word in words:
+    word_lengths[word] = len(word)
+  return word_lengths
+
+print(word_length_dictionary(["apple", "dog", "cat"]))
+print(word_length_dictionary(["a", ""]))
 
 
 # Frequency Count
