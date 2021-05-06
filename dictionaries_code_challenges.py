f297b10c62de02628f34f3131429b42be996b575
Kevin Botero <kbotero201@gmail.com>
2021-05-06 13:06:08 -0400

    freq cc completed

--- dictionaries_code_challenges.py
+++ dictionaries_code_challenges.py
@@ -18,7 +18,16 @@ print(word_length_dictionary(["a", ""]))
 # Write a function named frequency_dictionary that takes a list of elements named words as a parameter. 
 # The function should return a dictionary containing the frequency of each element in words.
 
+def frequency_dictionary(words):
+  freqs = {}
+  for word in words:
+    if word not in freqs:
+    	freqs[word] = 0
+    freqs[word] += 1
+  return freqs
 
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+print(frequency_dictionary([0,0,0,0,0]))
 
 # Unique Values
 # Create a function named unique_values that takes a dictionary named my_dictionary as a parameter. 
