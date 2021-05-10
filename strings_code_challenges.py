d99baed8299e9ec05e190d850437539590ad1347
Kevin Botero <kbotero201@gmail.com>
2021-05-10 10:36:29 -0400

    reverse cc

--- strings_code_challenges.py
+++ strings_code_challenges.py
@@ -33,6 +33,15 @@ print(every_other_letter(""))
 # Reverse 
 # Write a function named reverse_string that has a string named word as a parameter. The function should return word in reverse.
 
+def reverse_string(word):
+  reverse = ""
+  for i in range(len(word)-1, -1, -1):
+    reverse += word[i]
+  return reverse
+
+print(reverse_string("Codecademy"))
+print(reverse_string("Hello world!"))
+print(reverse_string(""))
 
 
 # Make Spoonerism
