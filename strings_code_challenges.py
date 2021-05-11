f6f59a9b749caedce4a136678a991eb58fe0d563
Kevin Botero <kbotero201@gmail.com>
2021-05-11 16:54:58 -0400

    spoonerism cc

--- strings_code_challenges.py
+++ strings_code_challenges.py
@@ -47,7 +47,12 @@ print(reverse_string(""))
 # Make Spoonerism
 # Write a function called make_spoonerism that takes two strings as parameters named word1 and word2. Finding the first syllable of a word is a difficult task, so for our function, we’re going to switch the first letters of each word. Return the two new words as a single string separated by a space.
 
+def make_spoonerism(word1, word2):
+  return word2[0]+word1[1:]+" "+word1[0]+word2[1:]
 
+print(make_spoonerism("Codecademy", "Learn"))
+print(make_spoonerism("Hello", "world!"))
+print(make_spoonerism("a", "b"))
 
 # Add Exclamation
 # Create a function named add_exclamation that has one parameter named word. 
