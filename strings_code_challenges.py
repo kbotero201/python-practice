85c52296aacd07fa80e5acf9c7fce607ed59184d
Kevin Botero <kbotero201@gmail.com>
2021-05-11 16:55:29 -0400

    excl cc

--- strings_code_challenges.py
+++ strings_code_challenges.py
@@ -59,3 +59,11 @@ print(make_spoonerism("a", "b"))
 # This function should add exclamation points to the end of word until word is 20 characters long. 
 # If word is already at least 20 characters long, just return word.
 
+def add_exclamation(word):
+  while(len(word) < 20):
+    word += "!"
+  return word
+
+print(add_exclamation("Codecademy"))
+print(add_exclamation("Codecademy is the best place to learn"))
+
